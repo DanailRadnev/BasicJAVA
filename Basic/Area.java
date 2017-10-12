561b62713a0d0078d5f58c19f1a69b41d4e793d1
Danail Radnev <danailradnev@gmail.com>
2017-10-12 20:02:32 +0300

    Some of my very first java code

---
+++ Basic/Area.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Area{
+	
+
+	public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter a = ");
+	double a= sc.nextDouble();
+	System.out.println("Enter b=");
+	double b= sc.nextDouble();
+	double s= a*b;
+	System.out.println("S= "+ s);
+	}
+	
+
+}
+	
